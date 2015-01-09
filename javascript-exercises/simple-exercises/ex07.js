20f1d818e1ee401d94d2b30e72cb54872a4bd855
Joe Olenick <jolenick1298@gmail.com>
2015-01-08 23:03:33 -0500

    working on ex07

--- javascript-exercises/simple-exercises/ex07.js
+++ javascript-exercises/simple-exercises/ex07.js
@@ -4,3 +4,23 @@ following fashion {"merry":"god", "christmas":"jul", "and":"och",
 "happy":gott", "new":"nytt", "year":"år"} and use it to translate your
 Christmas cards from English into Swedish.
 */
+
+
+var swedishCard = '';
+var translations = {
+  merry:    "god",
+  christmas:"jul",
+  and:      "och",
+  happy:    "gott",
+  new:      "nytt",
+  year:     "år"
+};
+var englishCard = "merry christmas and happy new year";
+var splitting = englishCard.split(" ");
+
+
+
+
+
+
+//console.log(splitting);
