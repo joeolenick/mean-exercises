7dba68f3c58e46e7b0ca8293489e54efdc55c0f3
Joe Olenick <jolenick1298@gmail.com>
2015-01-08 22:26:49 -0500

    Updates to Ex 3-10, not all finished.

--- javascript-exercises/simple-exercises/ex04.js
+++ javascript-exercises/simple-exercises/ex04.js
@@ -6,10 +6,23 @@ For example, translate("this is fun") should return the string "tothohisos isos
 fofunon"
 */
 
-var translate = function(text){
-  var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p',
-                    'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
-
-  
-
+var isVowel = function(char) {
+  if (!char || char.length > 1) throw new Error('Input ' + char + ' is invalid at isVowel()');
+  return !!~['a', 'e', 'i', 'o', 'u', 'y'].indexOf(char.toLowerCase());
 };
+
+// 'This is fun'
+var translate = function(word) {
+  if (!word) throw new Error('TRANSLATE(): Null passed to function translate');
+  var rovarspraketString = '';
+  for (var i = 0; i < word.length; i++) {
+    if (!isVowel(word[i]) && word[i] !== ' ') {
+      rovarspraketString += word[i] + 'o' + word[i];
+    } else {
+      rovarspraketString += word[i];
+    }
+  }
+  return rovarspraketString;
+};
+
+console.log(translate('this is fun'));
