3317c3f20b96497dbd8cb53a89f078a7a5d2222c
Joe Olenick <jolenick1298@gmail.com>
2015-01-08 20:14:36 -0500

    Ex 4-6

---
+++ javascript-exercises/simple-exercises/ex04.js
@@ -0,0 +1,15 @@
+
+/*
+4. Write a function translate() that will translate a text into "rövarspråket".
+That is, double every consonant and place an occurrence of "o" in between.
+For example, translate("this is fun") should return the string "tothohisos isos
+fofunon"
+*/
+
+var translate = function(text){
+  var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p',
+                    'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+
+  
+
+};
