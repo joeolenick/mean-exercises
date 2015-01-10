aeb744b4a38ddaf7fc2b1981928da6c8b6fde26c
Joe Olenick <jolenick1298@gmail.com>
2015-01-09 23:41:51 -0500

    Update to 3 and 6, finsihed 7.

--- javascript-exercises/simple-exercises/ex03.js
+++ javascript-exercises/simple-exercises/ex03.js
@@ -7,7 +7,13 @@ if it is a vowel, false otherwise.
 var identifyVowel = function(char){
   if(!char || char.length > 1)
     throw new Error("Input not invalid.")
-  else if(char.toLowerCase() === 'a' | 'e' | 'i' | 'o'| 'u' | 'y'){
+  else if(char.toLowerCase() === 'a' ||
+    char.toLowerCase() === 'e' ||
+    char.toLowerCase() === 'i'  ||
+    char.toLowerCase() === 'o'  ||
+    char.toLowerCase() === 'u'  ||
+    char.toLowerCase() === 'y'
+  ){
     return true;
   }else{
     return false;
@@ -15,5 +21,5 @@ var identifyVowel = function(char){
 };
 
 console.log(identifyVowel("a")); //returns true
-console.log(identifyVowel("A")); //returns true
+console.log(identifyVowel("E")); //returns true
 console.log(identifyVowel("d")); //returns false
