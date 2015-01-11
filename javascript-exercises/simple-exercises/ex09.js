f44b6f82be2f73b569010451109f51eff6b3d30f
Joe Olenick <jolenick1298@gmail.com>
2015-01-11 16:48:36 -0500

    Finished with all ex.

--- javascript-exercises/simple-exercises/ex09.js
+++ javascript-exercises/simple-exercises/ex09.js
@@ -2,3 +2,22 @@
 9. Write a function filterLongWords() that takes an array of words and an
 integer i and returns the array of words that are longer than i.
 */
+
+var array = [ "JoeOlenick",
+123,
+"Hippopotomonstrosesquippedaliophobia",
+"Test",
+"pneumonoultramicroscopicsilicovolcanoconiosis",
+"Oxyphenbutazone"];
+
+var filterLongWords = function(wordList, int){
+  if (!wordList) throw new Error('filterLongWords: Null entry');
+  var longest = [];
+  for (var i = 0; i < wordList.length; i++) {
+    if (wordList[i].length > int) {
+      longest.push(wordList[i]);
+    }
+  }
+  return longest;
+};
+console.log(filterLongWords(array, 10));
--- javascript-exercises/simple-exercises/ex10.js
+++ javascript-exercises/simple-exercises/ex10.js
@@ -4,3 +4,20 @@ listing of the characters contained in it. Represent the frequency listing
 as a Javascript object. Try it with something like
 charFreq("abbabcbdbabdbdbabababcbcbab").
 */
+
+
+var charFreq = function(string){
+  if (!string) throw new Error('charFreq: Null entry');
+    var charIterations = {};
+    for (var i = 0; i < string.length; i++){
+      if(string[i] in charIterations){
+          charIterations[string[i]] += +1;
+        }else{
+          charIterations[string[i]] = 1;
+      }
+      //console.log(charIterations); //Used for testing, fun to watch as well.
+    }
+    return charIterations;
+};
+
+console.log(charFreq("abbabcbdbabdbdbabababcbcbababbabcbdbabdbdbabababcbcbab"));
