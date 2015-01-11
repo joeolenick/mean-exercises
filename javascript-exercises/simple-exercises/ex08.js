1d528eabf5b5ec21187a8a4a461d51ac4465d418
Joe Olenick <jolenick1298@gmail.com>
2015-01-11 13:15:12 -0500

    finished ex08.js

--- javascript-exercises/simple-exercises/ex08.js
+++ javascript-exercises/simple-exercises/ex08.js
@@ -2,3 +2,20 @@
 8. Write a function findLongestWord() that takes an array of words and returns
 the length of the longest one.
 */
+
+var array = [ "JoeOlenick",
+              "Hippopotomonstrosesquippedaliophobia",
+              "Test",
+              "pneumonoultramicroscopicsilicovolcanoconiosis",
+              "Oxyphenbutazone"];
+var findLongestWord = function(wordList){
+  if (!wordList) throw new Error('findLongestWord: Null entry');
+  var longest = 0;
+  for (var i = 0; i < wordList.length; i++) {
+    if (wordList[i].length > longest) {
+      longest = wordList[i].length;
+    }
+  }
+  return longest;
+};
+console.log(findLongestWord(array));
