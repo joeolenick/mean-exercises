482ab54ff63a3009ba4e5d2e10b33bc81a47637f
Robert Williams <robert@increscent.org>
2015-01-15 14:34:02 -0700

    added sorting js file

---
+++ javascript-exercises/sorting/sorting.js
@@ -0,0 +1,6 @@
+/* Write a function that will sort an array like this one [1, 5, 9, 12, 4, 2, 8]
+ * You may have to search google for some simple sorting algorithms.
+ * (A solution is in the sorting-solution.js file)
+ */
+ 
+ var unsorted_array = [3, 5, 9, 12, 4, 2, 8];
