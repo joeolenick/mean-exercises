4d48c8d63b6fabb2a4ed288d537e31ac7b637eab
Joe Olenick <jolenick1298@gmail.com>
2015-01-21 22:24:01 -0500

    update

--- javascript-exercises/html-css-js-ajax/ajax/ajaxWeekendProject.js
+++ javascript-exercises/html-css-js-ajax/ajax/ajaxWeekendProject.js
@@ -6,20 +6,21 @@ requestContacts.open('GET','http://increscent.org:9000/contacts', true);
 requestContacts.onload = function(){
 
   var data = JSON.parse(requestContacts.response);
-  console.log(data[0]);
+
   for (var i = 0; i<data.length; i++){
-  var paragraphNode = document.createElement("p");
-  var text = document.createTextNode(data[i].name);
-  paragraphNode.appendChild(text);
-  document.body.appendChild(paragraphNode);
+  if (document.getElementsByClass('Contact')){
+    myElement = document.getElementById("1" + 1).innerHTML = data[i]["name"];
+  }
+
   }
 };
 //GET and display message from contacts.
 
 requestMessages.open('GET','http://increscent.org:9000/messages');
 requestMessages.onload = function() {
+
   var data = JSON.parse(requestMessages.response);
-  console.log(data[0]);
+
   for (var i = 0; i<data.length; i++){
     var paragraphNode = document.createElement("p");
     var text = document.createTextNode(data[i].text);
