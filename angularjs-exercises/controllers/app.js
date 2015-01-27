ec5f356c41c4ade3eb03b590977c9141af534bca
Robert Williams <robert@increscent.org>
2015-01-26 20:47:42 -0700

    added controllers exercise

---
+++ angularjs-exercises/controllers/app.js
@@ -0,0 +1,9 @@
+angular.module('profileApp', [])
+
+.controller('profileController', function ($scope) {
+  var profile_image = 'http://i.ytimg.com/vi/URTTK2V2CAE/hqdefault.jpg';
+  var name = 'Yzma';
+  var eye_color = 'blue';
+  var expression = 'surprise';
+  var thoughts = 'Is that my voice? Is that myyyyyy voice?';
+});
