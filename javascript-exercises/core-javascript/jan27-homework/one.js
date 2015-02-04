dcd4dac5d05ec2e7a667efae529fb9a00f4f094d
Joe Olenick <jolenick1298@gmail.com>
2015-02-03 21:44:55 -0500

    update to one

--- javascript-exercises/core-javascript/jan27-homework/one.js
+++ javascript-exercises/core-javascript/jan27-homework/one.js
@@ -1,15 +1,11 @@
-var container = [];
 var total = 0;
 
 for(var i=1; i<1000; i++){
    if(i % 3 === 0){
-      container.push(i);
+      total += i;
    }else if(i % 5 === 0){
-      container.push(i);
+      total += i;
    }
 }
-
-for (var i in container)
-   total += container[i];
 
 console.log(total);
