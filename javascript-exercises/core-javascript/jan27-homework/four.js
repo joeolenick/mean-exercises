6dba34f0f63e96938c93cd5c26c7b5d6fe931d62
Joe Olenick <jolenick1298@gmail.com>
2015-02-03 22:07:28 -0500

    adding four.js

---
+++ javascript-exercises/core-javascript/jan27-homework/four.js
@@ -0,0 +1,16 @@
+var num = 10;
+var firstLine = "";
+
+for(var count=0; count<num; count++){
+   firstLine =+ 'x'.toCharArray();
+}
+
+for(var lines = 0; lines < num -2; lines++){
+   var data = 'x';
+   for(var spaces = 0; space < num -2; spaces++){
+      data += ' '.toCharArray();
+   }
+}
+
+console.log(firstLine);
+console.log(data)
